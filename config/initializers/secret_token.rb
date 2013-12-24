0a683b65e50dabb8b03af2981a6337d24d6adb26
Clinton Judy <clinton@j-udy.com>
2013-12-24 00:12:21 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makespace::Application.config.secret_key_base = '3cf3e2c92cbc7d847ffd4ea7d58a6d1690529d54a87ce8bf5bdd82027b847b2073369527eb8c2b43b531cafc120ca5f259542e54ed16b9f364abcbf39e59ba3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Makespace::Application.config.session_store :cookie_store, key: '_makespace_session'
